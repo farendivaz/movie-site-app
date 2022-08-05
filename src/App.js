c5a7749d19f42fee907bbf8cd04cfee17b08c8f5
farendivaz <farendivaz@gmail.com>
2022-08-05 16:50:49 +0700

    change the submit and hide the api key

--- src/App.js
+++ src/App.js
@@ -1,67 +1,42 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 import MovieCard from "./components/MovieCard";
-import SearchIcon from "./Search.svg";
-const API_URL = "https://www.omdbapi.com?apikey=ec648708";
+const API_URL = `https://www.omdbapi.com?apikey=${process.env.REACT_APP_API_KEY}`;
 
 function App() {
   const [movies, setMovies] = useState([]);
   const [search, setSearch] = useState("");
 
   useEffect(() => {
-    getMoviesData("war");
+    getMoviesData();
+    // eslint-disable-next-line
   }, []);
 
-  const getMoviesData = async (title) => {
-    const res = await fetch(`${API_URL}&s=${title}`);
+  const getMoviesData = async (e) => {
+    e.preventDefault();
+    const url = `${API_URL}&s=${search}`;
+    const res = await fetch(url);
     const data = await res.json();
     setMovies(data.Search);
   };
 
-  const handleSubmit = () => {
-    getMoviesData(search);
-  };
-
-  useEffect(() => {
-    const keyDownHandler = (e) => {
-      if (e.key === "Enter") {
-        e.preventDefault();
-        handleSubmit();
-      }
-    };
-    document.addEventListener("keydown", keyDownHandler);
-
-    return () => {
-      document.removeEventListener("keydown", keyDownHandler);
-    };
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [() => handleSubmit()]);
-
   return (
     <div className="app">
       <h1>MovieApp</h1>
-      <form className="search" onSubmit={handleSubmit}>
+      <form className="search" onSubmit={getMoviesData}>
         <input
           placeholder="Search for movies"
           value={search}
           onChange={(e) => setSearch(e.target.value)}
         />
-        <img
-          src={SearchIcon}
-          alt="Search"
-          onClick={() => getMoviesData(search)}
-        />
+        <button>Go</button>
       </form>
 
-      {movies?.length > 0 ? (
+      {movies?.length > 0 && (
         <div className="container">
           {movies.map((movie) => (
             <MovieCard movie={movie} />
           ))}
-        </div>
-      ) : (
-        <div className="empty">
-          <h2>No movies found</h2>
         </div>
       )}
     </div>
